a798f041c09ced199383299b1dba909c24e7a4c6
harin park <73606877+khakaa@users.noreply.github.com>
2023-05-12 15:08:01 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -2,7 +2,6 @@ def solution(n, costs):
     answer = 0
     costs.sort(key = lambda x : x[2])
     link = set([costs[0][0]])
-    print(costs)
     while len(link) != n:
         for cost in costs:
             if cost[0] in link and cost[1] in link:
