cf9b36ae8b7897e2feec745a0d05c9e60bfc7cf5
khakaa <phr0210@naver.com>
2023-06-22 20:55:41 +0900

    테트로미노 풀이 추가

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -21,3 +21,52 @@ for i in range(n):
             ans = max(ans, total)
 
 print(ans)     
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+vis = [[0]*M for _ in range(N)]
+ans = 0
+dxy = [(0,1),(0,-1),(1,0),(-1,0)]
+
+# ㅓ,ㅏ,ㅗ,ㅜ 모양 제외한 dfs
+def dfs1(i, j, cnt, total):
+    global ans
+
+    if cnt == 4:
+        ans = max(ans, total)
+        return
+
+    for x,y in dxy:
+        nx, ny = x+i, y+j
+        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+            continue
+        if vis[nx][ny] == 0:
+            vis[nx][ny] = 1
+            dfs1(nx, ny, cnt + 1, total + board[nx][ny])
+            vis[nx][ny] = 0
+
+# ㅓ,ㅏ,ㅗ,ㅜ 모양 dfs
+def dfs2(x, y):
+    global ans
+
+    for i in range(4):
+        total = board[x][y]
+        for j in range(3):
+            t = (i+j)%4
+            nx,ny = x + dxy[t][0], y + dxy[t][1]
+            
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                break
+
+            total += board[nx][ny]
+        ans = max(ans, total)
+
+for i in range(N):
+    for j in range(M):
+        vis[i][j] = 1
+        dfs1(i,j,1,board[i][j]) 
+        vis[i][j] = 0
+
+        dfs2(i,j)
+
+print(ans)
