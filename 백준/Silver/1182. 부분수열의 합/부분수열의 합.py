e451089b42faa842fe0bce4a0a8ed592966753d4
Parkharin <73606877+khakaa@users.noreply.github.com>
2023-11-25 00:30:54 +0900

    [Silver II] Title: 부분수열의 합, Time: 356 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,21 +1,25 @@
-n, s = map(int ,input().split())
-number = list(map(int ,input().split()))
+import sys
+input = sys.stdin.readline
+N, S = map(int, input().split())
+seq = list(map(int, input().split()))
 answer = 0
 
-def dfs(i,total,number,visited):
-    global answer,n,s
-    if total == s:
+
+def dfs(i, total, visited):
+    global answer, N, S, seq
+    if total == S:
         answer += 1
 
-    for j in range(i+1,n):
+    for j in range(i+1, N):
         if visited[j] == 0:
             visited[j] = 1
-            dfs(j, total+number[j],number,visited)
+            dfs(j, total+seq[j], visited)
             visited[j] = 0
 
-for i in range(n):
-    visited = [0]*n
+
+for i in range(N):
+    visited = [0]*N
     visited[i] = 1
-    dfs(i,number[i],number,visited)
+    dfs(i, seq[i], visited)
 
 print(answer)
